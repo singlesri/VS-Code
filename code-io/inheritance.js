4b3681d49d266a1de4a2beb5aeb8b6cc8820392b
singlesri <singlesri2k@gmail.com>
2024-10-05 09:13:37 +0530

    inherit and override

--- code-io/inheritance.js
+++ code-io/inheritance.js
@@ -1,3 +1,24 @@
-class user {
-    
+class Animal {
+    legs;
+    tail;
+
+    constructor(legs, tail){
+        this.legs = legs;
+        this.tail = tail;
+    }
+
+    display(){
+        console.log(this.legs);
+        console.log(this.tail);
+    }
 }
+
+class Human extends Animal {
+    nationality;
+}
+
+let anime = new Animal (2,false);
+anime.display();
+
+let goms = new Human(4, false);
+console.log(goms);
