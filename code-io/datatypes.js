c3df9a31a537d877da373aa8d3414a6cf1ad1272
singlesri <singlesri2k@gmail.com>
2025-01-24 22:42:54 +0530

    datatypes

---
+++ code-io/datatypes.js
@@ -0,0 +1,24 @@
+let sum = 10 + 20;
+console.log(typeof sum);
+
+
+let bigInt = 239042397832939n;
+
+console.log(typeof bigInt);
+
+
+let Lrn = 12345678910n + 10n;
+
+console.log(Lrn + 2024n);
+
+
+// string datatype
+
+let str = "sample integer text";
+
+console.log(typeof str);
+
+
+let embd = `1+2 = ${1+2}`;
+
+console.log(embd);
