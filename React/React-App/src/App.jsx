3e84a72dba20915fcdc9e344799ad92d08a12cf4
singlesri <singlesri2k@gmail.com>
2025-06-03 22:15:47 +0530

    updates

--- React/React-App/src/App.jsx
+++ React/React-App/src/App.jsx
@@ -5,15 +5,13 @@ import Footer from './Footer';
 import Html from './assets/t1.jpeg';
 import tshirt from './assets/t2.jpeg';
 import tee from './assets/t3.jpeg'
+import CourseList from './CourseList';
 
 
 function App() {
   return (
     <>
-    <Course name="HTML" price="199$" image={Html} rating={5} show={true}/>
-    <Course name="Butcher" price="$150" image={tshirt} show={true}/>
-    <Course name="JSS" price="$300" image={tee} show={true}/>
-    <Course />
+      <CourseList />
     </>
   );
 }
--- React/React-App/src/CourseList.jsx
+++ React/React-App/src/CourseList.jsx
@@ -2,37 +2,46 @@ import Course from './Course';
 import Footer from './Footer';
 import Html from './assets/t1.jpeg';
 import tshirt from './assets/t2.jpeg';
-import tee from './assets/t3.jpeg'
+import tee from './assets/t3.jpeg';
 
-function CourseList(){
-
+function CourseList() {
     const courses = [
         {
-            name: "HTML", 
-            price: "199$", 
-            image: Html, 
-            rating: 5, 
-        },
-        {
-            name: "CSS", 
-            price: "99$", 
-            image: tshirt, 
+            id: 2,
+            name: "HTML",
+            price: "199$",
+            image: Html,
             rating: 5,
         },
         {
-            name: "JS", 
-            price: "299$", 
-            image: tee, 
+            id: 1,
+            name: "CSS",
+            price: "99$",
+            image: tshirt,
+            rating: 5,
+        },
+        {
+            id: 2,
+            name: "JS",
+            price: "299$",
+            image: tee,
             rating: 4,
         }
-    ]
+    ];
 
     const coursesList = courses.map(
-        (course) => 
-        <Course name={course.name) image={course.image} price = {course.price} />)
+        (course, index) => 
+        <Course key={index} name={course.name} 
+        image={course.image} 
+        price={course.price}
+        rating={course.rating} />
+    );
 
-    return(
+    return (
+        <>
+            {coursesList}
+        </>
     );
 }
 
-export default CourseList
+export default CourseList;
