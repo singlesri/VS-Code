b804cd56f9dad75026ef001c3bd8e9ddf69c3ea4
singlesri <singlesri2k@gmail.com>
2025-06-05 10:06:50 +0530

    click events handling

--- React/React-App/src/Course.jsx
+++ React/React-App/src/Course.jsx
@@ -1,15 +1,21 @@
-import PropTypes from 'prop-types';
+import PropTypes, { func } from 'prop-types';
 import HTML from './assets/t1.jpeg'
 
 const course1 = "HTML"
 
 function Course(props) {
 
+    function BuyCourse(discount, e) {
+        console.log(props.name, "purchased with", discount, "% discount");
+        console.log(e);
+    }
+
         return (
             props.name && <div className="card">
                 <img src={props.image} alt="" />
                 <h3>{props.name}</h3>
                 <p>{props.price}</p>
+                <button onClick={(event)=> {BuyCourse(20); console.log(event);}}>Buy Now</button><br /><br />
                 <span>{props.rating}</span> 
             </div>
         );
